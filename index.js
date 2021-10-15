ca760a8adad8045d7e0dd2a4ceff58c9d4f22a89
Maurice Smith <Xmaurice.smith@gmail.com>
2021-10-14 20:49:38 -0400

    commit tree 86d89db0d1d8ab9eb45793530c016b9de9be80de

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+//const drivers = ['Felix', 'Felix', 'Tyrese', 'Ayden']
+
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())        
+}
+
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(driver => driver.substring(0,name.length) === name) 
+}
+
+
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name)
+  
+  }
+
+    
+    // for(const user of array) {
+    //     if (user.firstName === string) {
+    //         return 
+    //         console.log(user.firstName)
+    //     }
+    // }
+    // return array.filter() 
+
